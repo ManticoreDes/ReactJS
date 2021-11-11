b7fd61e55f57b58aae6148b65224c058cd34f895
Manticore <ryanbaugh9@gmail.com>
2021-11-11 15:08:03 -0500

    pages and components

--- react-website-v1/src/App.js
+++ react-website-v1/src/App.js
@@ -1,14 +1,24 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
 import "./App.css";
+import Home from "./components/pages/HomePage/Home";
+import Services from "./components/pages/Services/Services";
+import Products from "./components/pages/Products/Products";
+import SignUp from "./components/pages/SignUp/SignUp";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/pages/Footer/Footer";
 
 function App() {
 	return (
 		<Router>
 			<Navbar />
-			<Switch>
-				<Route path="/" />
-			</Switch>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/services" element={<Services />} />
+				<Route path="/products" element={<Products />} />
+				<Route path="/sign-up" element={<SignUp />} />
+			</Routes>
+			<Footer />
 		</Router>
 	);
 }
